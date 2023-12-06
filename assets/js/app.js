403d6da12d2f5685570849a84dce223f04727141
Narli Tavahin <tavahinn@gmail.com>
2023-12-06 03:01:11 -0800

    update project section

--- assets/js/app.js
+++ assets/js/app.js
@@ -38,26 +38,3 @@ AOS.init({
 
 
 
-// var swiper = new Swiper(".mySwiper", {
-//     effect: "coverflow",
-//     grabCursor: true,
-//     centeredSlides: true,
-//     slidesPerView: "auto",
-//     coverflowEffect: {
-//       rotate: 50,
-//       stretch: 0,
-//       depth: 300,
-//       modifier: 1,
-//       slideShadows: true,
-//     },
-//     pagination: {
-//       el: ".swiper-pagination",
-//     },
-//     loop:true,
-//   });
-// //   Aos
-// AOS.init({
-//     duration: 1000,
-//     easing: "ease-in-out",
-// });
-
